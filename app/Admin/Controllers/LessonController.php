e94ce71b75ff88ab748911b0b1af8ea4ad53e993
kisah <data.kisahtegar@gmail.com>
2023-08-03 12:05:18 +0700

    Lessons Module : lesson migrations.

---
+++ app/Admin/Controllers/LessonController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Lesson;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class LessonController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Lesson';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Lesson());
+
+        $grid->column('id', __('Id'));
+        $grid->column('course_id', __('Course id'));
+        $grid->column('name', __('Name'));
+        $grid->column('thumbnail', __('Thumbnail'));
+        $grid->column('description', __('Description'));
+        $grid->column('video', __('Video'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Lesson::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('course_id', __('Course id'));
+        $show->field('name', __('Name'));
+        $show->field('thumbnail', __('Thumbnail'));
+        $show->field('description', __('Description'));
+        $show->field('video', __('Video'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Lesson());
+
+        $form->number('course_id', __('Course id'));
+        $form->text('name', __('Name'));
+        $form->text('thumbnail', __('Thumbnail'));
+        $form->textarea('description', __('Description'));
+        $form->textarea('video', __('Video'));
+
+        return $form;
+    }
+}
