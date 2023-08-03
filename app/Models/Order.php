e1765d4a5013e29816a2015c3715eb5d12600472
kisah <data.kisahtegar@gmail.com>
2023-08-03 11:54:37 +0700

    Create order module

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+    use DefaultDatetimeFormat; // this will used better time formatting
 }
